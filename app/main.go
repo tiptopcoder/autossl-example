3dc7e8b77de8ed296f3528d585f5942350275383
Tien Nhan <nhantien2806@gmail.com>
2021-05-03 14:10:56 -0400

    Getting domains from API

--- app/main.go
+++ app/main.go
@@ -12,11 +12,22 @@ import (
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", DoHealthCheck).Methods("GET")
+	router.HandleFunc("/domain", CheckDomain).Methods("GET")
 	log.Println("Golang server starts at localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 func DoHealthCheck(w http.ResponseWriter, r *http.Request) {
-	host := r.Header.Get("Host")
-	fmt.Fprintf(w, "Hello, i'm a golang microservice "+host)
+	log.Println(r.Host)
+	fmt.Fprintf(w, "Hello, i'm a golang microservice "+r.Host)
 	w.WriteHeader(http.StatusAccepted) //RETURN HTTP CODE 202
+}
+func CheckDomain(w http.ResponseWriter, r *http.Request) {
+	domain := r.URL.Query().Get("s")
+	log.Println(domain)
+
+	if domain == "cuiseene.com" {
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+	}
 }
